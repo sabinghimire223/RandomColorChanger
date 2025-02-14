a8deb1447fc21d801f9c75b060ab82b223e07a48
Sabin Ghimire <sabinghimire223@gmail.com>
2025-02-14 22:19:36 +0545

    made it so we can change text color too

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
+let startColor = document.getElementById("start2");
+let stopColor = document.getElementById("stop2");
+let intervalFunctionBG;
 let intervalFunction;
-let eventfunction = function () {
-  intervalFunction = setInterval(function () {
+let eventfunctionBG = function () {
+  intervalFunctionBG = setInterval(function () {
     let a = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     let c = Math.floor(Math.random() * 255);
     document.body.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
   }, 1000);
 };
-start.addEventListener("click", eventfunction, false);
+start.addEventListener("click", eventfunctionBG, false);
 stop.addEventListener(
+  "click",
+  () => {
+    clearInterval(intervalFunctionBG);
+  },
+  false
+);
+let eventfunction = function () {
+  intervalFunction = setInterval(function () {
+    let a = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let c = Math.floor(Math.random() * 255);
+    document.body.style.color = `rgb(${a}, ${b}, ${c})`;
+  }, 1000);
+};
+startColor.addEventListener("click", eventfunction, false);
+stopColor.addEventListener(
   "click",
   () => {
     clearInterval(intervalFunction);

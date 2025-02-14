d9ca69e66562c8b39ed91efba7bd0768b4eee8c4
Sabin Ghimire <sabinghimire223@gmail.com>
2025-02-14 21:30:02 +0545

    Completeing the remaing function

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
 let intervalFunction;
-
-start.addEventListener(
+let eventfunction = function () {
+  intervalFunction = setInterval(function () {
+    let a = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let c = Math.floor(Math.random() * 255);
+    document.body.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+  }, 1000);
+};
+start.addEventListener("click", eventfunction, false);
+stop.addEventListener(
   "click",
-  function () {
-    console.log("pressed");
-
-    interval = setInterval(function () {
-      let hexcode = Math.random().floor * 255;
-      document.body.style.backgroundColor = `#${hexcode}`;
-    }, 1);
+  () => {
+    clearInterval(intervalFunction);
   },
   false
 );
